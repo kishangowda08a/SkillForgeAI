2cdd77f4b0bff9fa7c887df4ab5746684ea02276
Kishan Gowda B N <kickskishan@gmail.com>
2026-08-11 22:51:11 +0530

    render deployment changes

--- client/kishan/src/services/api.js
+++ client/kishan/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://skillforgeai-26c3.onrender.com"
+  baseURL: "https://skillforgeai-26c3.onrender.com/api"
 });
 
 export default API;
